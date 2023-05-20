8ef6a685106d3b77179c7d8bb4d74752bfb95217
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-21 05:51:39 +0700

    Update addv.sh

--- menu/addv.sh
+++ menu/addv.sh
@@ -43,32 +43,31 @@ export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
 export BOLD="\e[1m"
 export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
-Repo="https://raw.githubusercontent.com/rizkihdyt6/asu/asu/"
-Repo1="https://raw.githubusercontent.com/rizkihdyt6/izinsc/main/"
+#Belum Kepikiran
 COLOR1='\033[0;35m'
 NC='\e[0m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-CEKEXPIRED () {
-    today=$(date -d +1day +%Y-%m-%d)
-    Exp1=$(curl -sS ${Repo1}p | grep $MYIP | awk '{print $3}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
-    else
-    echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
-    exit 0
-fi
-}
-IZIN=$(curl -sS ${Repo1}ip | awk '{print $4}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-CEKEXPIRED
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-exit 0
-fi
-clear
+#echo "Checking VPS"
+#CEKEXPIRED () {
+#    today=$(date -d +1day +%Y-%m-%d)
+#    Exp1=$(curl -sS ${Repo1}p | grep $MYIP | awk '{print $3}')
+#    if [[ $today < $Exp1 ]]; then
+#    echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
+#    else
+#    echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
+#    exit 0
+#fi
+#}
+#IZIN=$(curl -sS ${Repo1}ip | awk '{print $4}' | grep $MYIP)
+#if [ $MYIP = $IZIN ]; then
+#echo -e "\e[32mPermission Accepted...\e[0m"
+#CEKEXPIRED
+#else
+#echo -e "\e[31mPermission Denied!\e[0m";
+#exit 0
+#fi
+#clear
 
 source /var/lib/scrz-prem/ipvps.conf
 if [[ "$IP" = "" ]]; then
