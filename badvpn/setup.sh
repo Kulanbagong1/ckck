86414907ecaa747891187fa01c9228d75d6bf5c3
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-21 04:43:49 +0700

    Update setup.sh

--- badvpn/setup.sh
+++ badvpn/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-Repo="https://raw.githubusercontent.com/rizkihdyt6/asu/asu/"
-Repo1="https://raw.githubusercontent.com/rizkihdyt6/izinsc/main/"
+
 wget -O /usr/sbin/badvpn "${REPO}badvpn/badvpn" >/dev/null 2>&1
 chmod +x /usr/sbin/badvpn > /dev/null 2>&1
 wget -q -O /etc/systemd/system/badvpn1.service "${Repo}badvpn/badvpn1.service" >/dev/null 2>&1
